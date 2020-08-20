9fb1de1c19a38e0648d29b65f5f6e89e5b501eba
Bernd Schörgers <bernd@bjws.nl>
2020-08-20 10:24:38 +0200

    Refactor CI
    Only generate sealed-secrets on changed files by default, add linter step

--- .github/scripts/generate_sealedsecrets.sh
+++ .github/scripts/generate_sealedsecrets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 REPO_ROOT=$(git rev-parse --show-toplevel)
 CLUSTER_ROOT="${REPO_ROOT}/deployments"
 SECRETS_ROOT="${REPO_ROOT}/.secrets"
@@ -6,8 +8,21 @@ TEMPLATES_ROOT="${REPO_ROOT}/.templates"
 CLUSTER_VARS="${SECRETS_ROOT}/cluster-vars.yaml"
 PUB_CERT="${REPO_ROOT}/_setup/sealedsecret-cert.pem"
 
+if [[ -z $PRUNE_FILES ]]; then
+  PRUNE_FILES="false"
+fi
+
+while getopts i:r option
+do
+  case "${option}" in
+  i) INPUT_FILE=${OPTARG};;
+  r) PRUNE_FILES="true";;
+  *) echo "usage: $0 [-m] [-r]" >&2 && exit 1
+ esac
+done
+
 need() {
-    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+    command -v "$1" &>/dev/null || (echo "Binary '$1' is missing but required" && exit 1)
 }
 
 need "kubectl"
@@ -21,22 +36,42 @@ message() {
   echo "######################################################################"
 }
 
+if [ -n "$INPUT_FILE" ]; then
+  message "Processing files from $INPUT_FILE"
+  if [ -f "$INPUT_FILE" ]; then
+    FILES_TO_PROCESS=$(cat "$INPUT_FILE")
+  else
+    echo "File '$INPUT_FILE' does not exist. Aborting"
+    exit 1
+  fi
+else
+  FILES_TO_PROCESS=$(find "${SECRETS_ROOT}" \( -type f -name '*.yaml' -or -name '*.yml' \))
+fi
+
+# Validate cluster vars file
 [ -f "${CLUSTER_VARS}" ] || { echo >&2 "Cluster variables file doesn't exist. Aborting."; exit 1; }
 if ! yq validate "${CLUSTER_VARS}" > /dev/null 2>&1; then
   echo "Cluster variables file is invalid YAML. Aborting"
   exit 1
 fi
 
-message "Generating sealed-secrets"
+if [ $PRUNE_FILES == "true" ]; then
+  message "Generating sealed-secrets"
+else
+  message "Generating and pruning sealed-secrets"
+fi
 
-while IFS= read -r -d '' file
-do
-  # Get secret file metadata (path, filename, etc...)
-  secret_path="$(dirname "$file")"
-  secret_relative_path=${secret_path#"${SECRETS_ROOT}"}
-  secret_filename="$(basename "$file")"
-  secret_name=$(basename -s .yaml "$file")
-  secret_namespace=$(echo "$secret_relative_path" | awk -F/ '{print $2}')
+# Loop over the files that should be processed
+while IFS= read -r file; do
+  # Convert relative paths to absolute paths
+  if [[ "$file" != /* ]]; then
+    file="$REPO_ROOT/$file"
+  fi
+
+  # Only process files in the secrets folder
+  if [[ $file != ${SECRETS_ROOT}* ]]; then
+    continue
+  fi
 
   # Don't create a secret for the cluster-vars YAML
   if [[ "$file" == "$CLUSTER_VARS" ]]; then
@@ -44,15 +79,34 @@ do
   fi
 
   # Don't create any actual secrets for any sample YAMLs
-  if [[ "$file" == *sample.yaml ]]; then
+  if [[ $file == *sample.yaml ]]; then
     continue
   fi
 
-  echo "- Processing ${secret_relative_path}/${secret_filename}"
+  # Get secret file metadata (path, filename, etc...)
+  secret_filename="${file##*/}"
+  secret_path="${file%/$secret_filename}"
+  secret_relative_path=${secret_path#"${SECRETS_ROOT}"}
+
+  secret_name=${secret_filename%.*}
+  secret_namespace=$(echo "$secret_relative_path" | awk -F/ '{print $2}')
+  sealed_secret_path="${CLUSTER_ROOT}${secret_relative_path}"
+  sealed_secret_filename="${sealed_secret_path}/sealedsecret-${secret_name}.yaml"
+
+  echo "- Processing $file"
+
+  # Double check to see if the file exists
+  if [[ ! -f "$file" && $PRUNE_FILES == "true" ]]; then
+    if [[ -f "$sealed_secret_filename" ]]; then
+      echo "  Secret does not exist. Removing corresponding sealed-secret"
+      rm "$sealed_secret_filename"
+    fi
+    continue
+  fi
 
   # Make sure the file is valid YAML before we try to read it
   if ! yq validate "${file}" > /dev/null 2>&1; then
-    echo "${secret_relative_path}/${secret_filename} is invalid YAML. Aborting"
+    echo "  ${secret_relative_path}/${secret_filename} is invalid YAML. Aborting"
     exit 1
   fi
 
@@ -98,17 +152,18 @@ do
 
   # Make sure the generated secret is a valid Kubernetes Secret before proceeding
   if ! echo "$generated_secret" | kubeval --strict > /dev/null 2>&1; then
-    echo "Invalid secret generated for ${secret_relative_path}/${secret_filename}. Aborting"
+    echo "  Invalid secret generated for ${secret_relative_path}/${secret_filename}. Aborting"
     exit 1
   fi
 
   # Write out the actual sealed-secret and remove useless creationTimestamp
+  mkdir -p "${sealed_secret_path}"
+  echo "---" > "$sealed_secret_filename"
   echo "$generated_secret" | kubeseal --format=yaml --cert="${PUB_CERT}" \
     | \
   yq d - "metadata.creationTimestamp" \
     | \
-  yq d - "spec.template.metadata.creationTimestamp" > "${CLUSTER_ROOT}""${secret_relative_path}"/sealedsecret-"${secret_name}".yaml
-
-done <   <(find "${SECRETS_ROOT}" -name '*.yaml' -print0)
+  yq d - "spec.template.metadata.creationTimestamp" >> "$sealed_secret_filename"
+done <<< "$FILES_TO_PROCESS"
 
 message "all done!"
