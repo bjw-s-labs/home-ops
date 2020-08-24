9de911833ef4a03b92881b2e3599941edc6f3e88
bjws <bernd@bjws.nl>
2020-08-24 11:52:27 +0200

    Modify linting config

--- .github/scripts/lint_yaml.sh
+++ .github/scripts/lint_yaml.sh
@@ -2,7 +2,6 @@
 set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-SECRETS_ROOT="${REPO_ROOT}/.secrets"
 YAMLLINT_CONFIG="${REPO_ROOT}/.github/yamllint.config"
 
 need() {
@@ -19,22 +18,7 @@ message() {
 
 message "Running YAML lint on all YAML files in repository (except for .secrets folder)"
 
-FILES_TO_PROCESS=$(find "${REPO_ROOT}" -type d -path "$SECRETS_ROOT" -prune -o \( -type f -name '*.yaml' -or -name '*.yml' \) -print)
-
-# Loop over the files that should be processed
-set +e
-FAILED=0
-while IFS= read -r file; do
-  echo "- Processing $file"
-  yamllint -c "$YAMLLINT_CONFIG" "$file"
-  if [[ $? == 1 ]]; then
-    FAILED=1
-  fi
-done <<< "$FILES_TO_PROCESS"
-set -e
-
+yamllint -c "$YAMLLINT_CONFIG" "$REPO_ROOT"
+rv=$?
 message "all done!"
-
-if [[ $FAILED != 0 ]]; then
-  exit $FAILED
-fi
+exit $rv
