56cf24939fbdc9f75fd8117fe9530e4d4ec897ea
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <6213398+bjw-s@users.noreply.github.com>
2020-11-25 14:38:58 +0100

    Switch loops

--- .github/scripts/renovatebot-charts-annotation.sh
+++ .github/scripts/renovatebot-charts-annotation.sh
@@ -11,28 +11,25 @@ need() {
 
 need "yq"
 
-# loop thru and get all repository name and URLs
-for file in "${REPOSITORY_FILES}"/*.yaml; do
-    chart_name=$(yq r "$file" metadata.name)
-    chart_url=$(yq r "$file" spec.url)
+for helm_release in $(find ${CLUSTER_ROOT} -type f -name "*.yaml" ); do
+    # ignore flux-system namespace
+    # ignore wrong apiVersion
+    # ignore non HelmReleases
+    # ignore files that are not in $chart_name
+    if [[ "${helm_release}" =~ "system-flux"
+        || $(yq r "${helm_release}" apiVersion) != "helm.toolkit.fluxcd.io/v2beta1"
+        || $(yq r "${helm_release}" kind) != "HelmRelease" ]]; then
+        continue
+    fi
 
-    echo "$chart_name : $chart_url"
+    for file in "${REPOSITORY_FILES}"/*.yaml; do
+        chart_name=$(yq r "$file" metadata.name)
+        chart_url=$(yq r "$file" spec.url)
 
-    # loop thru all namespaces and update HelmReleases
-    for helm_release in $(find ${CLUSTER_ROOT} -type f -name "*.yaml" ); do
-        # ignore flux-system namespace
-        # ignore wrong apiVersion
-        # ignore non HelmReleases
-        # ignore files that are not in $chart_name
-        if [[ "${helm_release}" =~ "system-flux"
-            || $(yq r "${helm_release}" apiVersion) != "helm.toolkit.fluxcd.io/v2beta1"
-            || $(yq r "${helm_release}" kind) != "HelmRelease"
-            || $(yq r "${helm_release}" spec.chart.spec.sourceRef.name) != "${chart_name}" ]]; then
-            continue
+        if [[ $(yq r "${helm_release}" spec.chart.spec.sourceRef.name) == "${chart_name}" ]]; then
+            yq w -i "${helm_release}" 'metadata.annotations."renovatebot.helm.repository"' "${chart_url}"
+            echo "Annotated ${helm_release}"
+            break
         fi
-
-        yq w -i "${helm_release}" 'metadata.annotations."renovatebot.helm.repository"' "${chart_url}"
-
-        echo "Annotated ${helm_release}"
     done
 done
