597e9e7c2125ff1099b40b83773b5026841b8e99
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-02-15 09:10:15 +0100

    [workflow] Replace yq with sed/awk

--- .github/scripts/create-helmrelease-annotations.sh
+++ .github/scripts/create-helmrelease-annotations.sh
@@ -6,39 +6,35 @@ CLUSTER_ROOT="${REPO_ROOT}/cluster"
 NAMESPACES_ROOT="${CLUSTER_ROOT}/namespaces"
 REPOSITORY_FILES="${CLUSTER_ROOT}/base/system-flux/helm-chart-repositories"
 
-need() {
-    command -v "$1" &>/dev/null || (echo "Binary '$1' is missing but required" && exit 1)
-}
-
-need "yq"
-
-SED_COMMAND="sed"
-if type "gsed" > /dev/null; then
-  SED_COMMAND="gsed"
+# MacOS work-around for sed
+ if [ "$(uname)" == "Darwin" ]; then
+    # Check if gnu-sed exists
+    command -v gsed >/dev/null 2>&1 || {
+        echo >&2 "gsed is not installed. Aborting."
+        exit 1
+    }
+    # Export path w/ gnu-sed
+    export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 fi
 
 while IFS= read -r -d '' helm_release
 do
-    # ignore flux-system namespace
-    # ignore wrong apiVersion
-    # ignore non HelmReleases
-    if [[ "${helm_release}" =~ "system-flux"
-        || $(yq eval '.apiVersion' "${helm_release}") != "helm.toolkit.fluxcd.io/v2beta1"
-        || $(yq eval '.kind' "${helm_release}") != "HelmRelease" ]]; then
+    if ! grep -q "apiVersion: helm.toolkit.fluxcd.io/v2beta1" "${helm_release}" || ! grep -q "kind: HelmRelease" "${helm_release}"; then
         continue
     fi
 
-    for file in "${REPOSITORY_FILES}"/*.yaml; do
-        registry_name=$(yq eval '.metadata.name' "$file")
-        registry_url=$(yq eval '.spec.url' "$file")
+    helm_release_path=$(dirname "${helm_release}")
 
-        # ignore if the chart name does not match
-        if [[ $(yq eval '.spec.chart.spec.sourceRef.name' "${helm_release}") == "${registry_name}" ]]; then
+    for file in "${REPOSITORY_FILES}"/*.yaml; do
+        registry_name=$(awk '/metadata/{flag=1} flag && /name:/{print $NF;flag=""}' "$file")
+        registry_url=$(awk '/spec/{flag=1} flag && /url:/{print $NF;flag=""}' "$file")
+
+        if grep -q "name: ${registry_name}" "${helm_release}"; then
+            echo "Annotating HelmRelease ${helm_release_path#$NAMESPACES_ROOT/} with registry ${registry_name} for renovatebot..."
             # delete "renovate: registryUrl=" line
-            ${SED_COMMAND} -i "/renovate: registryUrl=/d" "${helm_release}"
+            sed -i "/renovate: registryUrl=/d" "${helm_release}"
             # insert "renovate: registryUrl=" line
-            ${SED_COMMAND} -i "/.*chart: .*/i \ \ \ \ \ \ # renovate: registryUrl=${registry_url}" "${helm_release}"
-            echo "Annotated helmrelease $(basename "$(dirname "${helm_release}")") with registry ${registry_name} for renovatebot..."
+            sed -i "/.*chart: .*/i \ \ \ \ \ \ # renovate: registryUrl=${registry_url}" "${helm_release}"
             break
         fi
     done
