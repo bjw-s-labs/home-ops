ddc7086045328e7090833db88b1d5d41a2937b88
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-07-19 15:15:46 +0200

    ci: Update workflow settings
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

---
+++ .github/renovate-config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  branchPrefix: "renovate/",
+  dryRun: true,
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  onboarding: false,
+  platform: "github",
+  includeForks: true,
+  repositories: ["bjw-s/k8s-gitops"],
+  requireConfig: true,
+  packageRules: [],
+};
