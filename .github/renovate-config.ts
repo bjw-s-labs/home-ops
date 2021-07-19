d47b4c5fe9836a81aea4b4b8cd6c01ea5acfba65
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-07-19 19:34:01 +0200

    ci: Update Renovate settings
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

--- .github/renovate-config.ts
+++ .github/renovate-config.ts
@@ -1,11 +1,11 @@
 module.exports = {
   branchPrefix: "renovate/",
-  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  gitAuthor: "Renovate Bot <87644328+bjw-s-renovate[bot]@users.noreply.github.com>",
   onboarding: false,
   platform: "github",
   includeForks: false,
   repositories: ["bjw-s/k8s-gitops"],
   requireConfig: true,
-  username: "bjw-s-renovate",
+  username: "bjw-s-renovate[bot]",
   packageRules: [],
 };
