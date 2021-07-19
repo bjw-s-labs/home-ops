9da3d7291e39847064df42dd6b8d960e93057eac
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-07-19 16:58:54 +0200

    ci: Update renovate
    Signed-off-by: Bernd Schorgers <me@bjw-s.dev>

--- .github/renovate-config.ts
+++ .github/renovate-config.ts
@@ -7,14 +7,5 @@ module.exports = {
   repositories: ["bjw-s/k8s-gitops"],
   requireConfig: true,
   username: "bjw-s-renovate",
-  hostRules: [
-    {
-      matchHost: "docker.io",
-      abortOnError: false,
-      abortIgnoreStatusCodes: [429],
-      username: process.env.DOCKER_HUB_USER,
-      token: process.env.DOCKER_HUB_PASSWORD,
-    },
-  ],
   packageRules: [],
 };
