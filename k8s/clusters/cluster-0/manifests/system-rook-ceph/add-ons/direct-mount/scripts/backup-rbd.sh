c14d7b6f811a229d5db16a72705e742b5b0ef3e7
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-11-03 20:00:17 +0100

    fix: Appease shellcheck linter
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

--- k8s/clusters/cluster-0/manifests/system-rook-ceph/add-ons/direct-mount/scripts/backup-rbd.sh
+++ k8s/clusters/cluster-0/manifests/system-rook-ceph/add-ons/direct-mount/scripts/backup-rbd.sh
@@ -8,9 +8,9 @@ application=""
 
 # Collect command line parameters
 while [ $# -gt 0 ]; do
-   if [[ $1 == *"--"* ]]; then
+   if [[ "$1" == *"--"* ]]; then
         param="${1/--/}"
-        declare $param="$2"
+        declare "$param"="$2"
    fi
   shift
 done
