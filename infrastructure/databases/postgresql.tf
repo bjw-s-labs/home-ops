08f32dcf9fe45b7a731ec20b99063a6d51b3e6e4
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-06 21:54:30 +0200

    feat(dsmr-reader): Migrate to external database

--- infrastructure/databases/postgresql.tf
+++ infrastructure/databases/postgresql.tf
@@ -1,3 +1,18 @@
+resource "postgresql_role" "dsmr-reader" {
+  name            = "dsmr-reader"
+  login           = true
+  password        = data.sops_file.database_secrets.data["postgres.roles.dsmr-reader.password"]
+}
+
+resource "postgresql_database" "dsmr-reader" {
+  name              = "dsmr-reader"
+  owner             = "dsmr-reader"
+  lc_collate        = "C"
+  connection_limit  = -1
+  allow_connections = true
+  depends_on = [postgresql_role.dsmr-reader]
+}
+
 resource "postgresql_role" "miniflux" {
   name            = "miniflux"
   login           = true
