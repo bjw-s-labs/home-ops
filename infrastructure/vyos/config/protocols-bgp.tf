e5b23b891ec489970f6d2ffdbe3b9c247ca7c333
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-07 15:07:12 +0200

    feat(vyos): Configure BGP

---
+++ infrastructure/vyos/config/protocols-bgp.tf
@@ -0,0 +1,22 @@
+resource "vyos_config_block_tree" "protocols-bgp" {
+  path = "protocols bgp"
+  configs = merge(
+    {
+      # main setup
+      "local-as"             = var.config.bgp.local_as
+      "parameters router-id" = var.config.bgp.router_id
+    },
+
+    merge(flatten([
+      for neighbour_group in var.config.bgp.neighbor_groups : [
+        for hostname, host in var.address_book.hosts : [
+          {
+            "neighbor ${cidrhost(var.networks[host.network], host.ipv4_hostid)} remote-as": neighbour_group.remote_as
+            "neighbor ${cidrhost(var.networks[host.network], host.ipv4_hostid)} description": hostname
+            "neighbor ${cidrhost(var.networks[host.network], host.ipv4_hostid)} address-family ipv4-unicast": ""
+          }
+        ] if contains(lookup(host, "groups", []), neighbour_group.group)
+      ]
+    ])...)
+  )
+}
