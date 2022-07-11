eee8874c060debea8d921226573f79288c5d812c
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-11 11:29:42 +0200

    feat(github): Use mineiros-io TF module

--- infrastructure/github/repositories.tf
+++ infrastructure/github/repositories.tf
@@ -1,89 +1,98 @@
-resource "github_repository" "home_ops" {
-  name        = "home-ops"
-  description = "My home or for-home infrastructure written as code, adhering to GitOps practices"
-  topics = [
-    "k8s-at-home",
-    "kubernetes",
-    "flux",
-    "gitops",
-    "renovate"
-  ]
+module "home-ops" {
+  source  = "mineiros-io/repository/github"
+  version = "0.16.2"
 
-  visibility = "public"
+  name         = "home-ops"
+  description  = "My home or for-home infrastructure written as code, adhering to GitOps practices"
+  topics       = ["flux", "gitops", "iac", "k8s-at-home", "kubernetes", "renovate"]
+  homepage_url = "https://bjw-s.github.io/home-ops"
+  visibility   = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
 
   has_issues   = true
   has_wiki     = false
   has_projects = false
   is_template  = false
 
-  allow_auto_merge       = true
+  plaintext_secrets = {
+    "BJWS_APP_ID"          = data.sops_file.github_secrets.data["apps.bjws_bot.app_id"]
+    "BJWS_APP_PRIVATE_KEY" = data.sops_file.github_secrets.data["apps.bjws_bot.private_key"]
+  }
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = [
+    { name = "area/ci", color = "72ccf3", description = "Issue relates to CI" },
+    { name = "area/kubernetes", color = "72ccf3", description = "Issue relates to Kubernetes" },
+
+    { name = "renovate/container", color = "ffc300", description = "Issue relates to a Renovate container update" },
+    { name = "renovate/helm", color = "ffc300", description = "Issue relates to a Renovate helm update" },
+
+    { name = "type/patch", color = "ffec19", description = "Issue relates to a patch version bump" },
+    { name = "type/minor", color = "ff9800", description = "Issue relates to a minor version bump" },
+    { name = "type/major", color = "f6412d", description = "Issue relates to a major version bump" },
+
+    { name = "size/XS", color = "009900", description = "Marks a PR that changes 0-9 lines, ignoring generated files" },
+    { name = "size/S", color = "77bb00", description = "Marks a PR that changes 10-29 lines, ignoring generated files" },
+    { name = "size/M", color = "eebb00", description = "Marks a PR that changes 30-99 lines, ignoring generated files" },
+    { name = "size/L", color = "ee9900", description = "Marks a PR that changes 100-499 lines, ignoring generated files" },
+    { name = "size/XL", color = "ee5500", description = "Marks a PR that changes 500-999 lines, ignoring generated files" },
+    { name = "size/XXL", color = "ee0000", description = "Marks PR that changes 1000+ lines, ignoring generated files" },
+
+    { name = "bug", color = "d73a4a", description = "Something isn't working" },
+    { name = "documentation", color = "0075ca", description = "Improvements or additions to documentation" },
+    { name = "enhancement", color = "a2eeef", description = "New feature or request" },
+    { name = "question", color = "d876e3", description = "Further information is requested" },
+    { name = "wontfix", color = "ffffff", description = "This will not be worked on" },
+    { name = "do-not-merge", color = "ee0701", description = "Marks PR that is not (yet) in a mergeable state" }
+  ]
+
+  pages = {
+    custom_404 = true
+    html_url   = "https://bjw-s.github.io/home-ops/"
+    status     = "built"
+    url        = "https://api.github.com/repos/bjw-s/home-ops/pages"
+    branch     = "gh-pages"
+    path       = "/"
+  }
+
+  webhooks = [
+    {
+      events       = ["push"]
+      url          = data.sops_file.github_secrets.data["home_ops.webhook.url"]
+      active       = true
+      content_type = "json"
+      secret       = data.sops_file.github_secrets.data["home_ops.webhook.secret"]
+    }
+  ]
+}
+
+module "pmb" {
+  source  = "mineiros-io/repository/github"
+  version = "0.16.2"
+
+  name        = "pmb"
+  description = "Poor Man's Backup"
+  topics      = ["kubernetes", "docker", "backups", "sidecar"]
+  visibility  = "public"
+
+  auto_init              = true
   allow_merge_commit     = false
-  allow_rebase_merge     = true
   allow_squash_merge     = true
+  allow_auto_merge       = true
   delete_branch_on_merge = true
 
-  homepage_url = "https://bjw-s.github.io/home-ops"
-  pages {
-    source {
-      branch = "gh-pages"
-      path   = "/"
-    }
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = {
+    "BJWS_APP_ID"          = data.sops_file.github_secrets.data["apps.bjws_bot.app_id"]
+    "BJWS_APP_PRIVATE_KEY" = data.sops_file.github_secrets.data["apps.bjws_bot.private_key"]
   }
-}
-
-resource "github_repository_environment" "home_ops_gh_pages" {
-  environment = "github-pages"
-  repository  = github_repository.home_ops.name
-
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = true
-  }
-}
-
-resource "github_repository_environment" "home_ops_production" {
-  environment = "production"
-  repository  = github_repository.home_ops.name
-
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = true
-  }
-}
-
-resource "github_actions_secret" "home_ops_bjws_bot_app_id" {
-  repository      = github_repository.home_ops.name
-  secret_name     = "BJWS_APP_ID"
-  plaintext_value = data.sops_file.github_secrets.data["apps.bjws_bot.app_id"]
-}
-
-resource "github_actions_secret" "home_ops_bjws_bot_app_key" {
-  repository      = github_repository.home_ops.name
-  secret_name     = "BJWS_APP_PRIVATE_KEY"
-  plaintext_value = data.sops_file.github_secrets.data["apps.bjws_bot.private_key"]
-}
-
-resource "github_actions_secret" "home_ops_terraform_token" {
-  repository  = github_repository.home_ops.name
-  secret_name = "TF_CLOUD_TOKEN"
-}
-
-resource "github_actions_environment_secret" "home_ops_production_terraform_token" {
-  repository  = github_repository.home_ops.name
-  environment = github_repository_environment.home_ops_production.environment
-  secret_name = "TF_CLOUD_TOKEN"
-}
-
-resource "github_repository_webhook" "home_ops_flux" {
-  repository = github_repository.home_ops.name
-  active     = true
-  events     = ["push"]
-
-  configuration {
-    url          = data.sops_file.github_secrets.data["home_ops.webhook.url"]
-    content_type = "json"
-    secret       = data.sops_file.github_secrets.data["home_ops.webhook.secret"]
-    insecure_ssl = false
-  }
-
 }
