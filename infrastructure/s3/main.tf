443766ba9062f15e9a09b282ec92ff4170302e73
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-08-05 11:45:55 +0200

    feat(s3): Manage S3 buckets with Terraform

---
+++ infrastructure/s3/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "bjw-s"
+    workspaces {
+      name = "home-s3-provisioner"
+    }
+  }
+
+  required_providers {
+    b2 = {
+      source = "Backblaze/b2"
+      version = "0.8.1"
+    }
+    minio = {
+      source = "aminueza/minio"
+      version = "1.5.3"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.1"
+    }
+  }
+}
+
+data "sops_file" "s3_secrets" {
+  source_file = "secret.sops.yaml"
+}
+
+data "sops_file" "domains" {
+  source_file = pathexpand("${path.module}/../domains.sops.yaml")
+}
