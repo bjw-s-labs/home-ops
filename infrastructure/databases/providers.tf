2884cab926bac0822d58d7cd0fd5b86fa4f66866
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-01 13:12:26 +0200

    feat(postgres)!: Remove terraform code

--- infrastructure/databases/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "postgresql" {
-  host            = "vm-docker-01.${local.domains.hardware}"
-  port            = 5432
-  database        = "postgres"
-  username        = "postgres"
-  password        = data.sops_file.database_secrets.data["postgres.postgres_password"]
-  sslmode         = "disable"
-  connect_timeout = 90000000
-}
