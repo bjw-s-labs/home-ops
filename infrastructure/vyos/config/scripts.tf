9b9dd82969fc8cd1f1f7e528e9ad222e6ebd50c3
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-22 08:55:21 +0200

    fix(vyos): Workaround to clean up stale network entries

--- infrastructure/vyos/config/scripts.tf
+++ infrastructure/vyos/config/scripts.tf
@@ -4,3 +4,11 @@ resource "remote_file" "scripts-precommit-add_container_images" {
   content     = file(pathexpand("${path.module}/../scripts/commit/pre-hooks.d/add-container-images.sh"))
   permissions = "0755"
 }
+
+resource "remote_file" "scripts-preconfig-bootup" {
+  provider = remote
+  path     = "/config/scripts/vyos-preconfig-bootup.script"
+  content = file(
+    pathexpand("${path.module}/../scripts/vyos-preconfig-bootup.script"))
+  permissions = "0755"
+}
