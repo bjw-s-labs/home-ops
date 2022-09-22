c102bc729363bb4937b76b62aa58cb3698c4a7db
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-22 13:58:10 +0200

    fix(github): Revert provider version to 4.31.0

--- infrastructure/github/main.tf
+++ infrastructure/github/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "4.31.0"
     }
     sops = {
       source  = "carlpett/sops"
--- infrastructure/github/repositories.tf
+++ infrastructure/github/repositories.tf
@@ -271,3 +271,30 @@ module "gh-workflows" {
     local.default_issue_labels
   )
 }
+
+module "klipper-config" {
+  source  = "mineiros-io/repository/github"
+  version = "0.16.2"
+
+  name         = "klipper-config"
+  description  = "My Klipper configurations"
+  topics       = ["3dprinter", "klipper"]
+  visibility   = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [],
+    local.default_issue_labels
+  )
+}
