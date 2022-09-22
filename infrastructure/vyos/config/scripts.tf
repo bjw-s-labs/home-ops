3f61953e8a0ba0c5f5c485a89a9f6bf20e7724cd
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-22 13:10:48 +0200

    fix(vyos): Fix pre-commit script

--- infrastructure/vyos/config/scripts.tf
+++ infrastructure/vyos/config/scripts.tf
@@ -1,8 +1,10 @@
 resource "remote_file" "scripts-precommit-add_container_images" {
   provider    = remote
-  path        = "/config/scripts/commit/pre-hooks.d/add-container-images.sh"
-  content     = file(pathexpand("${path.module}/../scripts/commit/pre-hooks.d/add-container-images.sh"))
-  permissions = "0755"
+  path        = "/config/scripts/commit/pre-hooks.d/add-container-images"
+  content     = file(pathexpand("${path.module}/../scripts/commit/pre-hooks.d/add-container-images"))
+  permissions = "0774"
+  owner       = "root"
+  group       = "vyattacfg"
 }
 
 resource "remote_file" "scripts-preconfig-bootup" {
@@ -10,5 +12,7 @@ resource "remote_file" "scripts-preconfig-bootup" {
   path     = "/config/scripts/vyos-preconfig-bootup.script"
   content = file(
     pathexpand("${path.module}/../scripts/vyos-preconfig-bootup.script"))
-  permissions = "0755"
+  permissions = "0774"
+  owner       = "root"
+  group       = "vyattacfg"
 }
