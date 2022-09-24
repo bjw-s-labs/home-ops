85ba8bf51fe31d777085f2c12abcfe7e5048fbb5
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-24 10:56:12 +0200

    feat(vyos): Add vpn dyndns

--- infrastructure/vyos/config/service-dns-dynamic.tf
+++ infrastructure/vyos/config/service-dns-dynamic.tf
@@ -2,6 +2,15 @@ resource "vyos_config_block_tree" "service-dns-dynamic" {
   path = "service dns dynamic"
 
   configs = merge(
+    {
+      "interface eth0 service vpn host-name" = "vpn.${var.domains["ingress"]}"
+      "interface eth0 service vpn server"    = "api.cloudflare.com/client/v4"
+      "interface eth0 service vpn protocol"  = "cloudflare"
+      "interface eth0 service vpn zone"      = "${var.domains["ingress"]}"
+      "interface eth0 service vpn login"     = "${var.secrets.cloudflare.login}"
+      "interface eth0 service vpn password"  = "${var.secrets.cloudflare.key}"
+    },
+
     merge([
       for domain in ["family", "personal", "ingress"] : {
         "interface eth0 service ${domain} host-name" = "gateway.${var.domains[domain]}"
