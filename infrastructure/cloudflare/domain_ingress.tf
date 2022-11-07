88ff641669ff699708c4b959207029ee36c8d6fc
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-11-07 11:28:51 +0100

    feat(cloudflare): Add DNS records

--- infrastructure/cloudflare/domain_ingress.tf
+++ infrastructure/cloudflare/domain_ingress.tf
@@ -1,7 +1,99 @@
 module "cf_domain_ingress" {
   source     = "./cf_domain"
-  domain     = data.sops_file.cloudflare_secrets.data["cloudflare_zones.ingress"]
+  domain     = local.domains["ingress"]
   account_id = cloudflare_account.bjw_s.id
+  dns_entries = [
+    {
+      name  = "ipv4"
+      value = local.home_ipv4
+    },
+    {
+      name  = "ingress"
+      value = "ipv4.${local.domains["ingress"]}"
+      type  = "CNAME"
+    },
+    {
+      name    = "vpn"
+      value   = "ipv4.${local.domains["ingress"]}"
+      type    = "CNAME"
+      proxied = false
+    },
+    # Generic settings
+    {
+      name  = "_dmarc"
+      value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s"
+      type  = "TXT"
+    },
+    # Fastmail settings
+    {
+      id       = "fastmail_mx_1"
+      name     = "@"
+      priority = 10
+      value    = "in1-smtp.messagingengine.com"
+      type     = "MX"
+    },
+    {
+      id       = "fastmail_mx_2"
+      name     = "@"
+      priority = 20
+      value    = "in2-smtp.messagingengine.com"
+      type     = "MX"
+    },
+    {
+      id      = "fastmail_dkim_1"
+      name    = "fm1._domainkey"
+      value   = "fm1.${local.domains["ingress"]}.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id      = "fastmail_dkim_2"
+      name    = "fm2._domainkey"
+      value   = "fm2.${local.domains["ingress"]}.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id      = "fastmail_dkim_3"
+      name    = "fm3._domainkey"
+      value   = "fm3.${local.domains["ingress"]}.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id    = "fastmail_spf"
+      name  = "@"
+      value = "v=spf1 include:spf.messagingengine.com ?all"
+      type  = "TXT"
+    },
+    # Mailgun settings
+    {
+      id       = "mailgun_mx_1"
+      name     = "mg"
+      priority = 10
+      value    = "mxa.eu.mailgun.org"
+      type     = "MX"
+    },
+    {
+      id       = "mailgun_mx_2"
+      name     = "mg"
+      priority = 50
+      value    = "mxb.eu.mailgun.org"
+      type     = "MX"
+    },
+    {
+      id    = "mailgun_dkim_1"
+      name  = "mta._domainkey.mg"
+      value = "k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA01bXkhzb3ANETaXVWO2ODDiakyOoBLS+JfOBqKr70QG40jcUi4QXg2FTWx6pn2fo1enXCm9YIFglSjaJaWp+QBJ9vWIE6ALzKzuE+MKLNSZhaoi8fdGkSM8SC/qXJxcckIpmgVpzA/V89OGZ1rcEKWZqfaSH49KwSyHEWXgZy//G7LQot5NNwPNdJxh9V+nQfd/NxP2qR2t378iX2nE2Rcwgv+mx1ccv73Cy5TeE1+rsag3kTCJTMikB4oJa+tWqr+rav23Z4aYiAZSQyrh/ccQzZGizLtEvZB/OU+IFe36BYdGuBO5N4Ca/rJ/onm72qZcfCmlH1jT0zRBGygxNOQIDAQAB"
+      type  = "TXT"
+    },
+    {
+      id    = "mailgun_spf"
+      name  = "mg"
+      value = "v=spf1 include:mailgun.org ~all"
+      type  = "TXT"
+    },
+  ]
 }
 
 resource "cloudflare_filter" "cf_domain_ingress_github_flux_webhook" {
@@ -9,7 +101,7 @@ resource "cloudflare_filter" "cf_domain_ingress_github_flux_webhook" {
   description = "Allow GitHub flux API"
   expression = format(
     "(ip.geoip.asnum eq 36459 and http.host eq \"flux-receiver-cluster-0.%s\")",
-    data.sops_file.cloudflare_secrets.data["cloudflare_zones.ingress"]
+    local.domains["ingress"]
   )
 }
 
