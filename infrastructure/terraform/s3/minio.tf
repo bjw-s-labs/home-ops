e94589e883c4542b2b672cc78001a54cdc0af046
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-15 22:01:29 +0100

    feat(minio): Hook up Terraform to 1Password

--- infrastructure/terraform/s3/minio.tf
+++ infrastructure/terraform/s3/minio.tf
@@ -1,9 +1,24 @@
+data "onepassword_vault" "services" {
+  name = "Services"
+}
+
+data "onepassword_item" "minio" {
+  vault = data.onepassword_vault.services.uuid
+  title = "Minio"
+}
+
+locals {
+  minio_users = data.onepassword_item.minio.section[index(data.onepassword_item.minio.section.*.label, "Other users")].field
+}
+
 module "minio_bucket_thanos" {
   source      = "./modules/minio_bucket"
   bucket_name = "thanos"
   providers = {
     minio = minio.nas
   }
+  user_name   = local.minio_users[index(local.minio_users.*.label, "thanos_access_key")].value
+  user_secret = local.minio_users[index(local.minio_users.*.label, "thanos_secret_key")].value
 }
 
 module "minio_bucket_loki" {
@@ -12,6 +27,8 @@ module "minio_bucket_loki" {
   providers = {
     minio = minio.nas
   }
+  user_name   = local.minio_users[index(local.minio_users.*.label, "loki_access_key")].value
+  user_secret = local.minio_users[index(local.minio_users.*.label, "loki_secret_key")].value
 }
 
 module "minio_bucket_postgresql" {
@@ -20,6 +37,8 @@ module "minio_bucket_postgresql" {
   providers = {
     minio = minio.nas
   }
+  user_name   = local.minio_users[index(local.minio_users.*.label, "postgresql_access_key")].value
+  user_secret = local.minio_users[index(local.minio_users.*.label, "postgresql_secret_key")].value
 }
 
 module "minio_bucket_volsync" {
@@ -28,4 +47,6 @@ module "minio_bucket_volsync" {
   providers = {
     minio = minio.nas
   }
+  user_name   = local.minio_users[index(local.minio_users.*.label, "volsync_access_key")].value
+  user_secret = local.minio_users[index(local.minio_users.*.label, "volsync_secret_key")].value
 }
