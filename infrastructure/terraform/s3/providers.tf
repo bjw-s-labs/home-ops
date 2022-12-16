a3bd09fc2465d36beec2288c4c224ef3671e1f3b
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-16 11:39:25 +0100

    feat(s3): Make external password a bit nicer to work with

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias          = "nas"
   minio_server   = data.sops_file.s3_secrets.data["minio_server"]
-  minio_user     = data.onepassword_item.minio.username
-  minio_password = data.onepassword_item.minio.password
+  minio_user     = local.minio_admin_user
+  minio_password = local.minio_admin_password
 }
