624f95bcb19ef2981620f3839e299782789e921c
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-16 09:35:19 +0100

    feat(terraform/tf-controller): Set up 1PW connect

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -4,8 +4,3 @@ provider "minio" {
   minio_user     = data.onepassword_item.minio.username
   minio_password = data.onepassword_item.minio.password
 }
-
-provider "onepassword" {
-  url = data.sops_file.s3_secrets.data["1pwconnect_url"]
-  token = data.sops_file.s3_secrets.data["1pwconnect_token"]
-}
