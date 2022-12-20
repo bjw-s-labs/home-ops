888990bc17d2611e6aafd0c91bd2a570086709f6
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-20 14:36:47 +0100

    feat(S3): use 1Password connect module

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias          = "nas"
-  minio_server   = data.sops_file.s3_secrets.data["minio_server"]
-  minio_user     = local.minio_admin_user
-  minio_password = local.minio_admin_password
+  minio_server   = "s3.bjw-s.dev"
+  minio_user     = module.onepassword_item_minio.fields.username
+  minio_password = module.onepassword_item_minio.fields.password
 }
