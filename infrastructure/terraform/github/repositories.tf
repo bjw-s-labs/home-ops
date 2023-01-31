0b4ccff9a898bc9126a72d2d17857d34bad6517d
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-01-31 08:26:16 +0100

    feat(github): Manage additional repo

--- infrastructure/terraform/github/repositories.tf
+++ infrastructure/terraform/github/repositories.tf
@@ -364,3 +364,35 @@ module "terraform-1password-item" {
     local.default_issue_labels
   )
 }
+
+module "series-cleanup" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "series-cleanup"
+  description = "Script to periodically clean media files from disk"
+  topics      = ["media", "golang", "trakt"]
+  visibility  = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = merge(
+    {},
+    local.bjws_bot_secrets
+  )
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [],
+    local.default_issue_labels
+  )
+}
