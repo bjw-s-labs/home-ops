cb725c4bddcd1ed135ab62e197adc59b439a877c
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-02-19 20:38:35 +0100

    feat(minio)!: Remove in favor of Garage

--- infrastructure/terraform/s3/modules/minio_bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secret_key" {
-  value     = minio_iam_user.user.secret
-  sensitive = true
-}
--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,13 +1,6 @@
-provider "minio" {
-  alias          = "nas"
-  minio_server   = "s3.bjw-s.dev"
-  minio_user     = module.onepassword_item_minio.fields.username
-  minio_password = module.onepassword_item_minio.fields.password
-}
-
 provider "garage" {
   alias  = "nas"
-  host   = "nas.bjw-s.tech:3911"
-  scheme = "http"
+  host   = "garage.bjw-s.dev"
+  scheme = "https"
   token  = module.onepassword_item_garage.fields.admin_api_token
 }
