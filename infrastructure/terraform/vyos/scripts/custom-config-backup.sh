9aa6e603aece53b8a61863fe26c40c459ce35e38
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-02-19 20:10:56 +0100

    feat(vyos): Update backup scripts

--- infrastructure/terraform/vyos/scripts/custom-config-backup.sh
+++ infrastructure/terraform/vyos/scripts/custom-config-backup.sh
@@ -7,6 +7,17 @@ dest=/media/usb-backup
 
 # Only backup if $dest is a mount
 if mountpoint -q $dest; then
-    # Backup /config
-    tar --exclude="overlay*" -zvcf $dest/config.$(date +%Y%m%d%H%M%S).tar.gz /config
+    # Backup # VyOS /config
+    backup_dest="$dest/vyos"
+    if [ ! -d "$backup_dest" ]; then
+        mkdir "$backup_dest"
+    fi
+    tar --exclude="overlay*" --exclude="unifi*" -zvcf "$backup_dest/config.$(date +%Y%m%d%H%M%S).tar.gz" /config
+
+    # Unifi backups
+    backup_dest="$dest/unifi"
+    if [ ! -d "$backup_dest" ]; then
+        mkdir "$backup_dest"
+    fi
+    tar -zvcf "$backup_dest/unifi-backup.$(date +%Y%m%d%H%M%S).tar.gz" /config/unifi/data/data/backup
 fi
