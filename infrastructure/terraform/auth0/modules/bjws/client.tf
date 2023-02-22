28ec3199723b643ebb40ed6b122f9a56d877a9e3
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-02-22 08:22:56 +0100

    fix(auth0): Update callback URLs

--- infrastructure/terraform/auth0/modules/bjws/client.tf
+++ infrastructure/terraform/auth0/modules/bjws/client.tf
@@ -9,7 +9,7 @@ resource "auth0_client" "default" {
     "https://grafana.bjw-s.dev/login/generic_oauth",        # Grafana
     "http://localhost:8000",                                # kubectl oidc-login
     "https://status.bjw-s.dev/authorization-code/callback", # Gatus
-    "https://kubenav.io/oidc.html",                         # Kubenav
+    "https://kubenav.io/auth/oidc.html",                    # Kubenav
     "https://gitops.bjw-s.dev/oauth2/callback",             # Weave-Gitops
   ]
 
