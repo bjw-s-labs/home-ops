d1e5156223d46a8adf791ce67b945ebadbe54b24
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-02-23 13:41:40 +0100

    feat(vyos)!: Remove terraform in favor of bash glue

--- infrastructure/terraform/vyos/modules/config/service-ssh.tf
+++
@@ -1,7 +0,0 @@
-resource "vyos_config" "service-ssh" {
-  path = "service ssh"
-  value = jsonencode({
-    "port"                            = "22"
-    "disable-password-authentication" = {}
-  })
-}
