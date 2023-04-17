1bfaa7acc96ab88577af99984b2069fdc3f6f2a3
Bernd Schorgers <me@bjw-s.dev>
2023-04-17 13:16:14 +0200

    feat(cloudflare): Update config

--- infrastructure/terraform/cloudflare/modules/cf_domain/firewall.tf
+++ infrastructure/terraform/cloudflare/modules/cf_domain/firewall.tf
@@ -4,15 +4,15 @@ resource "cloudflare_filter" "countries" {
   count = var.enable_default_firewall_rules ? 1 : 0
 
   zone_id     = cloudflare_zone.zone.id
-  description = "Expression to block all countries except NL"
-  expression  = "(ip.geoip.country ne \"NL\")"
+  description = "Expression to block all countries except NL/BE/DE"
+  expression  = "(ip.geoip.country ne \"NL\") and (ip.geoip.country ne \"BE\") and (ip.geoip.country ne \"DE\")"
 }
 
 resource "cloudflare_firewall_rule" "countries" {
   count = var.enable_default_firewall_rules ? 1 : 0
 
   zone_id     = cloudflare_zone.zone.id
-  description = "Firewall rule to block all countries except NL"
+  description = "Firewall rule to block all countries except NL/BE/DE"
   filter_id   = cloudflare_filter.countries[count.index].id
   action      = "block"
 }
