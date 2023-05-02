1a0042353eccfa40e2e747475c7b76f0757ae5ce
Bernd Schorgers <me@bjw-s.dev>
2023-05-02 15:53:47 +0200

    feat(cloudflare): Use 1PW instead of SOPS

--- infrastructure/terraform/cloudflare/locals.tf
+++ infrastructure/terraform/cloudflare/locals.tf
@@ -1,4 +1,3 @@
 locals {
-  cloudflare_secrets = sensitive(yamldecode(nonsensitive(data.sops_file.cloudflare_secrets.raw)))
-  home_ipv4          = chomp(data.http.ipv4_lookup_raw.response_body)
+  home_ipv4 = chomp(data.http.ipv4_lookup_raw.response_body)
 }
--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -22,11 +22,13 @@ terraform {
   }
 }
 
-data "sops_file" "cloudflare_secrets" {
-  source_file = "cloudflare_secrets.sops.yaml"
-}
-
 # Obtain current home IP address
 data "http" "ipv4_lookup_raw" {
   url = "http://ipv4.icanhazip.com"
+}
+
+module "onepassword_item_cloudflare" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "Services"
+  item   = "Cloudflare"
 }
