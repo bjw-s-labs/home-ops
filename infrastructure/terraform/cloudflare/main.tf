2449fb1b918ca5d16a83cd9e3acb99f89b0368fb
Bernd Schorgers <me@bjw-s.dev>
2023-05-05 08:44:57 +0200

    chore(cloudflare): Remove unused provider

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -11,10 +11,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "4.5.0"
     }
-    sops = {
-      source  = "carlpett/sops"
-      version = "0.7.2"
-    }
     http = {
       source  = "hashicorp/http"
       version = "3.3.0"
