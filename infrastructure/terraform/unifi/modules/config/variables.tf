d07f24b763db821db6998d34e7fcf1ebcc0239f0
Bernd Schorgers <me@bjw-s.dev>
2023-05-11 15:07:59 +0200

    feat(unifi): Update config

--- infrastructure/terraform/unifi/modules/config/variables.tf
+++ infrastructure/terraform/unifi/modules/config/variables.tf
@@ -2,3 +2,36 @@ variable "networks" {
   description = "managed networks"
   type        = any
 }
+
+variable "wlan_main_ssid" {
+  description = "Main WLAN SSID"
+  type        = string
+}
+
+variable "wlan_main_password" {
+  description = "Main WLAN password"
+  type        = string
+  sensitive   = true
+}
+
+variable "wlan_iot_ssid" {
+  description = "IoT WLAN SSID"
+  type        = string
+}
+
+variable "wlan_iot_password" {
+  description = "IoT WLAN password"
+  type        = string
+  sensitive   = true
+}
+
+variable "wlan_guest_ssid" {
+  description = "Guest WLAN SSID"
+  type        = string
+}
+
+variable "wlan_guest_password" {
+  description = "Guest WLAN password"
+  type        = string
+  sensitive   = true
+}
