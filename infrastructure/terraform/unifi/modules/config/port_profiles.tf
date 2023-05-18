09ddc1f162814321beba4ec3dd80839d592a0500
Bernd Schorgers <me@bjw-s.dev>
2023-05-18 08:28:47 +0200

    Revert "feat(unifi): Update config"
    This reverts commit 2dc71f0b0db8281ee1c6203314620e79ae654e1d.

--- infrastructure/terraform/unifi/modules/config/port_profiles.tf
+++ infrastructure/terraform/unifi/modules/config/port_profiles.tf
@@ -30,3 +30,15 @@ resource "unifi_port_profile" "iot_poe_disabled" {
   native_networkconf_id = unifi_network.iot.id
   poe_mode              = "off"
 }
+
+resource "unifi_port_profile" "k8s_server" {
+  name = "k8s-server"
+  site = unifi_site.default.name
+
+  forward               = "customize"
+  native_networkconf_id = unifi_network.servers.id
+  poe_mode              = "off"
+  tagged_networkconf_ids = [
+    unifi_network.iot.id
+  ]
+}
