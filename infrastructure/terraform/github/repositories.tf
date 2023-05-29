400580e6692cb908d2c93f7ccd541ba0822a3653
Bernd Schorgers <me@bjw-s.dev>
2023-05-29 15:01:16 +0200

    feat(github): Add repo

--- infrastructure/terraform/github/repositories.tf
+++ infrastructure/terraform/github/repositories.tf
@@ -591,3 +591,35 @@ module "dotfiles" {
     local.default_issue_labels
   )
 }
+
+module "helm3_unittest" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "helm3-unittest"
+  description = "Fork of lrills/helm-unittest but for Helm 3"
+  topics      = ["helm", "unittest", "golang"]
+  visibility  = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = merge(
+    {},
+    local.bjws_bot_secrets
+  )
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [],
+    local.default_issue_labels
+  )
+}
