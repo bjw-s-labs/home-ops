3ce101b9198cf0e56a4ff0bb17352bf60122db2c
Bernd Schorgers <me@bjw-s.dev>
2023-06-06 13:59:15 +0200

    feat(github): Remove asdf-kubeconform repo

--- infrastructure/terraform/github/repositories.tf
+++ infrastructure/terraform/github/repositories.tf
@@ -557,38 +557,6 @@ module "asdf_talhelper" {
   )
 }
 
-module "asdf_kubeconform" {
-  source  = "mineiros-io/repository/github"
-  version = "0.18.0"
-
-  name        = "asdf-kubeconform"
-  description = "kubeconform plugin for the asdf version manager"
-  topics      = ["asdf", "kubernetes", "kubeconform"]
-  visibility  = "public"
-
-  auto_init              = true
-  allow_merge_commit     = false
-  allow_squash_merge     = true
-  allow_auto_merge       = true
-  delete_branch_on_merge = true
-
-  has_issues   = true
-  has_wiki     = false
-  has_projects = false
-  is_template  = false
-
-  plaintext_secrets = merge(
-    {},
-    local.bjws_bot_secrets
-  )
-
-  issue_labels_merge_with_github_labels = false
-  issue_labels = concat(
-    [],
-    local.default_issue_labels
-  )
-}
-
 module "asdf_revive" {
   source  = "mineiros-io/repository/github"
   version = "0.18.0"
