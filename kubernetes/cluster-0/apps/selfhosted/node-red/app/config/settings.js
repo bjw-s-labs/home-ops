2a59f6274ddbfc32951a8e01b82031c3aecfee2c
Bernd Schorgers <me@bjw-s.dev>
2023-07-25 20:11:07 +0200

    feat(node-red): OIDC auth

---
+++ kubernetes/cluster-0/apps/selfhosted/node-red/app/config/settings.js
@@ -0,0 +1,85 @@
+module.exports = {
+  flowFile: "flows.json",
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+  flowFilePretty: true,
+
+  adminAuth: {
+    type: "strategy",
+    strategy: {
+      name: "openidconnect",
+      autoLogin: true,
+      label: "Sign in",
+      icon: "fa-cloud",
+      strategy: require("passport-openidconnect").Strategy,
+      options: {
+        issuer: "https://auth.bjw-s.dev",
+        authorizationURL: "https://auth.bjw-s.dev/api/oidc/authorization",
+        tokenURL: "https://auth.bjw-s.dev/api/oidc/token",
+        userInfoURL: "https://auth.bjw-s.dev/api/oidc/userinfo",
+        clientID: "nodered",
+        clientSecret: process.env.NODE_RED_OAUTH_CLIENT_SECRET,
+        callbackURL: "https://nodered.bjw-s.dev/auth/strategy/callback",
+        scope: ["email", "profile", "openid"],
+        proxy: true,
+        verify: function (issuer, profile, done) {
+          done(null, profile)
+        },
+      },
+    },
+    users: [{ username: "bernd", permissions: ["*"] }],
+  },
+
+  uiPort: process.env.PORT || 1880,
+
+  diagnostics: {
+    enabled: true,
+    ui: true,
+  },
+
+  runtimeState: {
+    enabled: false,
+    ui: false,
+  },
+
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false,
+    },
+  },
+
+  contextStorage: {
+    default: {
+      module: "localfilesystem",
+    },
+  },
+
+  exportGlobalContextKeys: false,
+
+  externalModules: {},
+
+  editorTheme: {
+    tours: false,
+
+    projects: {
+      enabled: false,
+      workflow: {
+        mode: "manual",
+      },
+    },
+
+    codeEditor: {
+      lib: "monaco",
+      options: {},
+    },
+  },
+
+  functionExternalModules: true,
+  functionGlobalContext: {},
+
+  debugMaxLength: 1000,
+
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+}
