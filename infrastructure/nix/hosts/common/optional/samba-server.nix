3feb4b0fc82f39f5d1eed4c382d9dc779c9ae7a9
Bernd Schorgers <me@bjw-s.dev>
2023-10-24 19:13:21 +0200

    feat(nix): Add NAS, update NAS-VM

--- infrastructure/nix/hosts/common/optional/samba-server.nix
+++ infrastructure/nix/hosts/common/optional/samba-server.nix
@@ -1,9 +1,20 @@
 {
+  users.groups.samba-users = {};
+
+  services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
   services.samba = {
     enable = true;
+    openFirewall = true;
     securityType = "user";
     extraConfig = ''
       workgroup = WORKGROUP
+      guest account = nobody
+      map to guest = bad user
+      veto files = /._*/.DS_Store/
+      delete veto files = yes
+      inherit acls = yes
+      map acl inherit = yes
+      valid users = @samba-users
     '';
   };
 }
