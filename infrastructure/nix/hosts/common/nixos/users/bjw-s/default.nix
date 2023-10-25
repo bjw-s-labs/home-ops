e48f251b64940f6be96f411c7b0adba6b6ef51a1
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2023-10-25 19:20:15 +0200

    fix(nix): Update iOS SSH key

--- infrastructure/nix/hosts/common/nixos/users/bjw-s/default.nix
+++ infrastructure/nix/hosts/common/nixos/users/bjw-s/default.nix
@@ -15,7 +15,7 @@ in
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMyYn4k4V+myBBl79Nt3t7EZugvz9A+d3ZbKyaP1w7J5 Bernd personal"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBciX/nvTebsm25TIqTHj+LGPbUb3bbZG23I/rrCMCRK Bernd iOS"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINllIKQjpMumg9CCz1HIEsti/cN6MpUWZbCeLiLjKH2W Bernd iOS"
     ];
   };
 }
