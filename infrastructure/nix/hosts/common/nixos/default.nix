d4e7e29e30789f316b955d65578533f3420b3dca
Bernd Schorgers <me@bjw-s.dev>
2023-10-28 14:45:38 +0200

    feat(nix): Update NAS, add SOPS

--- infrastructure/nix/hosts/common/nixos/default.nix
+++ infrastructure/nix/hosts/common/nixos/default.nix
@@ -1,6 +1,7 @@
 { inputs, outputs, ... }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
     ./locale.nix
     ./nix.nix
     ./packages.nix
