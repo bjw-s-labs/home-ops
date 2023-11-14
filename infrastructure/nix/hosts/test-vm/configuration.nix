26a6a5c775d3258d97b78caa1732cb9a158d82c1
Bernd Schorgers <me@bjw-s.dev>
2023-11-14 13:06:13 +0100

    fix(nix): Convert hosts to modules

--- infrastructure/nix/hosts/test-vm/configuration.nix
+++ infrastructure/nix/hosts/test-vm/configuration.nix
@@ -1,29 +1,22 @@
 { ... }:
 
 {
-  modules = {
-    device = {
-      cpu = "vm";
-      gpu = "vm";
-      hostId = "0e542f34";
+  config = {
+    modules = {
+      device = {
+        cpu = "vm";
+        gpu = "vm";
+        hostId = "0e542f34";
+      };
+
+      users.bjw-s.enable = true;
+
+      system.openssh.enable = true;
+      system.qemu-guest-agent.enable = true;
     };
 
-    users.bjw-s.enable = true;
-
-    system.openssh.enable = true;
-    system.qemu-guest-agent.enable = true;
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
   };
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
 }
