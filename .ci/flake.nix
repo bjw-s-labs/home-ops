8b5fc82299184c52218262ce412f4ca756a10e6f
Bernd Schorgers <me@bjw-s.dev>
2023-12-17 15:31:26 +0100

    ci: Switch homebrew with nix

---
+++ .ci/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "CI Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+    # System-specific logic
+    flake-utils.lib.eachDefaultSystem
+      (system:    # Non-system-specific logic
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell
+            {
+              buildInputs = (with pkgs; [
+                fluxcd
+                jo
+              ]);
+            };
+        };
+      });
+}
