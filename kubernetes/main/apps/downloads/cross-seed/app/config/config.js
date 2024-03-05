97c28f7f24531ccdc14c7e17fab8cd2272aee56b
Bernd Schorgers <me@bjw-s.dev>
2024-03-05 09:22:51 +0100

    chore(cross-seed): Update config

---
+++ kubernetes/main/apps/downloads/cross-seed/app/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  delay: 20,
+  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local:8080",
+  torznab: [
+    "http://prowlarr.downloads.svc.cluster.local:9696/5/api?apikey={{ .prowlarr_api_key }}",
+    "http://prowlarr.downloads.svc.cluster.local:9696/6/api?apikey={{ .prowlarr_api_key }}",
+    "http://prowlarr.downloads.svc.cluster.local:9696/13/api?apikey={{ .prowlarr_api_key }}",
+    "http://prowlarr.downloads.svc.cluster.local:9696/15/api?apikey={{ .prowlarr_api_key }}",
+    "http://prowlarr.downloads.svc.cluster.local:9696/18/api?apikey={{ .prowlarr_api_key }}",
+    "http://prowlarr.downloads.svc.cluster.local:9696/16/api?apikey={{ .prowlarr_api_key }}",
+  ],
+  action: "inject",
+  includeEpisodes: true,
+  includeNonVideos: true,
+  duplicateCategories: true,
+  matchMode: "safe",
+  skipRecheck: true,
+  linkType: "hardlink",
+  linkDir: "/data/nas-media/Downloads/qbittorrent/complete/cross-seed",
+  dataDirs: [
+    "/data/nas-media/Downloads/qbittorrent/complete/radarr",
+    "/data/nas-media/Downloads/qbittorrent/complete/sonarr",
+  ],
+  outputDir: "/config/xseeds",
+  torrentDir: "/config/qBittorrent/BT_backup",
+}
