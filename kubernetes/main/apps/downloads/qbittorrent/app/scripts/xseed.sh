38e5c9af1d86218d2f8ba9f4f8b5fb79e4cb4a46
Bernd Schorgers <me@bjw-s.dev>
2024-08-05 21:41:04 +0200

    fix(sabnzbd): Silence curl output

--- kubernetes/main/apps/downloads/qbittorrent/app/scripts/xseed.sh
+++ kubernetes/main/apps/downloads/qbittorrent/app/scripts/xseed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "${1}"
-curl -X POST --data-urlencode "path=${1}" "http://cross-seed.downloads.svc.cluster.local:2468/api/webhook?apikey=${CROSS_SEED__API_KEY}"
+curl -X POST --silent --data-urlencode "path=${1}" "http://cross-seed.downloads.svc.cluster.local:2468/api/webhook?apikey=${CROSS_SEED__API_KEY}"
