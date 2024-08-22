ef2c955f61de0034caa44f9e5368b58c9002a092
Bernd Schorgers <me@bjw-s.dev>
2024-08-22 11:50:12 +0200

    ci(lint): Add prettier

--- .github/actions/reviewdog-prettier/script.sh
+++ .github/actions/reviewdog-prettier/script.sh
@@ -19,7 +19,7 @@ echo "::group::📝 Running prettier with reviewdog 🐶 ..."
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-prettier --check ${INPUT_PRETTIER_FLAGS} 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 File is not properly formatted./' \
+"$(npm root)"/.bin/prettier --check ${INPUT_PRETTIER_FLAGS} 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 File is not properly formatted./' \
 | reviewdog \
   -efm="%-G[warn] Code style issues found in the above file(s). Forgot to run Prettier%. File is not properly formatted." \
   -efm="[%tarn] %f %m" \
