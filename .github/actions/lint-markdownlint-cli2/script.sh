633a9bd29216e44efef1712db378c44cc5c306c1
Bernd Schorgers <me@bjw-s.dev>
2024-08-22 15:37:59 +0200

    ci(lint): Update

--- .github/actions/lint-markdownlint-cli2/script.sh
+++ .github/actions/lint-markdownlint-cli2/script.sh
@@ -5,7 +5,7 @@ INPUT_MARKDOWNLINT_FLAGS=${INPUT_MARKDOWNLINT_FLAGS:=.}
 
 echo "::group::📝 Running markdownlint-cli2 ..."
 
-markdownlint-cli2 "${INPUT_MARKDOWNLINT_FLAGS}" ; exit_code=$?
+markdownlint-cli2 ${INPUT_MARKDOWNLINT_FLAGS} ; exit_code=$?
 
 echo "::endgroup::"
 exit ${exit_code}
