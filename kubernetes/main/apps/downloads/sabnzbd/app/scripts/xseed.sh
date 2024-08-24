7c675fa1e3a1541e3c23672aa61969c3f764f2ea
Bernd Schorgers <me@bjw-s.dev>
2024-08-24 17:11:43 +0200

    fix(cross-seed): Simplify script, yoink from Devin

--- kubernetes/main/apps/downloads/sabnzbd/app/scripts/xseed.sh
+++ kubernetes/main/apps/downloads/sabnzbd/app/scripts/xseed.sh
@@ -2,36 +2,26 @@
 
 XSEED_HOST=${XSEED_HOST:-crossseed}
 XSEED_PORT=${XSEED_PORT:-8080}
+XSEED_APIKEY=${XSEED_APIKEY:-unset}
+XSEED_SLEEP_INTERVAL=${CROSS_SEED_SLEEP_INTERVAL:-30}
 
 SEARCH_PATH=$1
 
-# Function to log messages
-log_message() {
-    local log_type="$1"
-    local message="$2"
-    local timestamp
-    timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
-    echo "${timestamp} [${log_type}] ${message}"
-}
+response=$(curl \
+  --silent \
+  --output /dev/null \
+  --write-out "%{http_code}" \
+  --request POST \
+  --data-urlencode "path=${SEARCH_PATH}" \
+  --header "X-Api-Key: ${XSEED_APIKEY}" \
+  "http://${XSEED_HOST}:${XSEED_PORT}/api/webhook"
+)
 
-# Function to send a request to Cross Seed API
-cross_seed_request() {
-    local endpoint="$1"
-    local data="$2"
-    local headers=(-X POST "http://${XSEED_HOST}:${XSEED_PORT}/api/${endpoint}" --data-urlencode "${data}")
-    if [[ -n "${XSEED_APIKEY}" ]]; then
-        headers+=(-H "X-Api-Key: ${XSEED_APIKEY}")
-    fi
-    response=$(curl --silent --output /dev/null --write-out "%{http_code}" "${headers[@]}")
-    echo "${response}"
-}
-
-xseed_resp=$(cross_seed_request "webhook" "path=${SEARCH_PATH}")
-
-if [[ "${xseed_resp}" == "204" ]]; then
-  log_message "INFO" "Process completed successfully."
-  sleep 30
-else
-  log_message "ERROR" "Process failed with API response: ${xseed_resp}"
+if [[ "${response}" != "204" ]]; then
+  printf "Failed to search cross-seed for '%s'\n" "${SEARCH_PATH}"
   exit 1
 fi
+
+printf "Successfully searched cross-seed for '%s'\n" "${SEARCH_PATH}"
+
+sleep "${XSEED_SLEEP_INTERVAL}"
