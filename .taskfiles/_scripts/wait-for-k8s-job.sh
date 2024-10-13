eab4b27961a12675d6d270f6c908e976750993ec
Bernd Schorgers <me@bjw-s.dev>
2024-10-13 15:32:46 +0200

    chore(taskfile): More streamlining

--- .taskfiles/_scripts/wait-for-k8s-job.sh
+++ .taskfiles/_scripts/wait-for-k8s-job.sh
@@ -2,12 +2,11 @@
 
 JOB_NAME=$1
 NAMESPACE="${2:-default}"
-CLUSTER=$3
 
 [[ -z "${JOB_NAME}" ]] && echo "Job name not specified" && exit 1
 
 while true; do
-    STATUS="$(kubectl --context "${CLUSTER}" -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
+    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
     if [[ "${STATUS}" == "Pending" ]]; then
         break
     fi
